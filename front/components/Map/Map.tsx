36b02464b966193dde4a9efeee098cb792c9ad8b
bmangust <ftf.pollux@gmail.com>
2021-11-28 14:59:44 +0300

    add Map

--- front/components/Map/Map.tsx
+++ front/components/Map/Map.tsx
@@ -1,6 +1,7 @@
-import React from "react";
-
+import React, { useState } from "react";
 import styled from "styled-components";
+import GoogleMapReact from "google-map-react";
+import { googleMapStyles } from "styles/googleMap.styles";
 
 const StyledDiv = styled.div`
   background-color: #fafafa;
@@ -13,7 +14,27 @@ interface Props {
 }
 
 const Map = ({ className }: Props) => {
-  return <StyledDiv className={className} />;
+  const [coordinates, setCoordinates] = useState({ lat: 55.75, lng: 37.64 });
+  return (
+    <StyledDiv className={className}>
+      <GoogleMapReact
+        bootstrapURLKeys={{
+          key: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API || "",
+        }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={11}
+        margin={[50, 50, 50, 50]}
+        options={{
+          disableDefaultUI: true,
+          zoomControl: true,
+          styles: googleMapStyles,
+        }}
+        onChange={() => {}}
+        onChildClick={() => {}}
+      ></GoogleMapReact>
+    </StyledDiv>
+  );
 };
 
 export default Map;
